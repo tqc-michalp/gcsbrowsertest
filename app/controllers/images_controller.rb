23ca23c3a795552f679b2d0c31fde6d817d7afbe
tqc-michalp <michal.pawlak@thinqcraft.com>
2023-03-22 23:03:53 +0100

    images#show.format=jpg download an image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ImagesController < ApplicationController
+  include ApplicationHelper
   def index
     collection = StorageWrapper.new.fetch_images.map do |image|
       ImageFacade.new(image)
@@ -10,10 +11,17 @@ class ImagesController < ApplicationController
   end
 
   def show
-    @image = ImageFacade.new(
-      StorageWrapper.new.fetch_by_file_name(
-        Base64.urlsafe_decode64(params[:name])
-      )
+    gcp_file = StorageWrapper.new.fetch_by_file_name(
+      Base64.urlsafe_decode64(params[:name])
     )
+    @image = ImageFacade.new(gcp_file)
+
+    respond_to do |format|
+      format.html
+      format.jpeg do
+        send_data(gcp_file.download.read, filename: "#{file_prefix}_image_#{gcp_file.name[..12]}.jpeg",
+                                          type: 'image/jpeg')
+      end
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,11 +28,11 @@ require 'webmock/rspec'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
-end
+# begin
+#   ActiveRecord::Migration.maintain_test_schema!
+# rescue ActiveRecord::PendingMigrationError => e
+#   abort e.to_s.strip
+# end
 
 VCR.configure do |cfg|
   cfg.allow_http_connections_when_no_cassette = true
