52d1196bdd07475d0ed3324fb66cbe15229ff18b
tqc-michalp <michal.pawlak@thinqcraft.com>
2023-03-22 21:57:22 +0100

    images#show to display image

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 
 Rails.application.routes.draw do
   root 'images#index'
-  resources :images, only: %i[index]
+  resources :images,
+            only: %i[index show],
+            param: :name
 end
